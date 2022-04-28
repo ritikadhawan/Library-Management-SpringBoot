c2669fac839aba17793caf4b3c214663ec4826e4
ritikadhawan <ritikadhawan28@gmail.com>
2022-04-28 11:30:56 +0530

    Refactor code

--- src/main/java/com/example/library/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/library/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.example.library.exception;
 
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +12,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -27,9 +24,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         List<String> errors = new ArrayList<>();
         errors.add(ex.getMessage());
 
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), errors);
-
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @Override
